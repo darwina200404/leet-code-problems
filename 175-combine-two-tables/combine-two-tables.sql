f6c2b3e37f61b1c146d78ce24c90afc64e7be703
Darwin_A <168660469+darwina200404@users.noreply.github.com>
2024-08-04 20:30:55 +0530

    Time: 417 ms (30.27%) | Memory: 0B (100.00%) - LeetSync

---
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -0,0 +1,7 @@
+SELECT person.firstName,
+       person.lastName,
+       address.city,
+       address.state
+FROM Person person
+LEFT OUTER JOIN Address address 
+ON person.personId = address.personId;
