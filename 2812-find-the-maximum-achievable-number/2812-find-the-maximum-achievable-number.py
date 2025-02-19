ca8ef44d2d3b806e701f96895e6e8810a42b1c74
Darwin_A <168660469+darwina200404@users.noreply.github.com>
2025-02-19 17:13:34 +0530

    Time: 0 ms (100%), Space: 12.5 MB (8.21%) - LeetHub

---
+++ 2812-find-the-maximum-achievable-number/2812-find-the-maximum-achievable-number.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def theMaximumAchievableX(self, num, t):
+        """
+        :type num: int
+        :type t: int
+        :rtype: int
+        """
+        t=t*2
+        return num+t
+        
