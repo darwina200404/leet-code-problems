4f414a2bdc0ff64a4ddcde5ea97d08f7c46bf777
Darwin_A <168660469+darwina200404@users.noreply.github.com>
2025-05-03 13:26:16 +0530

    Time: 0 ms (100%), Space: 12.6 MB (15.27%) - LeetHub

--- 2917-count-pairs-whose-sum-is-less-than-target/2917-count-pairs-whose-sum-is-less-than-target.py
+++ 2917-count-pairs-whose-sum-is-less-than-target/2917-count-pairs-whose-sum-is-less-than-target.py
@@ -6,12 +6,15 @@ class Solution(object):
         :rtype: int
         """
         count=0
-        for i in range(len(nums)):
-            sum=0
-            for j in range (i):
-                sum=nums[i]+nums[j]
-                if(sum<target):
-                    count+=1
+        nums.sort()
+        n=len(nums)-1
+        left=0
+        right=n
+        while left<right:
+            if ((nums[left]+nums[right])<target):
+                count+=right-left
+                left+=1
+            else:
+                right-=1
         return count
 
-        
