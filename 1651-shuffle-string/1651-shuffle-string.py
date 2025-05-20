900cca326093e334a48f22fc88fd073f9f1e69dd
Darwin_A <168660469+darwina200404@users.noreply.github.com>
2025-05-20 11:57:33 +0530

    Time: 2 ms (53.13%), Space: 12.4 MB (83.07%) - LeetHub

---
+++ 1651-shuffle-string/1651-shuffle-string.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def restoreString(self, s, indices):
+        """
+        :type s: str
+        :type indices: List[int]
+        :rtype: str
+        """
+        ans=[0]*len(s)
+        res=""
+        for i in range(len(s)):
+            ans[indices[i]]=s[i]
+        return "".join(ans)
+            
+
+
+        
+        
