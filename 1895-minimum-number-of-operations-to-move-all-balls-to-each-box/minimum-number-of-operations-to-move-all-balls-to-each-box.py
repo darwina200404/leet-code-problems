7f0927b6bbce016dd5255020b4e27e6b2dd13b87
Darwin_A <168660469+darwina200404@users.noreply.github.com>
2025-05-20 11:03:59 +0530

    Time: 27 ms (75.32%) | Memory: 12.7 MB (91.23%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.py
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -5,16 +5,25 @@ class Solution(object):
         :rtype: List[int]
         """
         n = len(boxes)
-        result = [0] * n
-
-        for i in range(n):
-            for j in range(n):
-                if boxes[j] == '1':
-                    result[i] += abs(i - j)
-        return result
+        res = [0] * n
         
+        count = 0
+        ops = 0
+        
+        for i in range(n):
+            res[i] += ops
+            count += int(boxes[i])
+            ops += count
 
-       
+        count = 0
+        ops = 0
+        
+        for i in range(n-1, -1, -1):
+            res[i] += ops
+            count += int(boxes[i])
+            ops += count
+        
+        return res
 
 
 
