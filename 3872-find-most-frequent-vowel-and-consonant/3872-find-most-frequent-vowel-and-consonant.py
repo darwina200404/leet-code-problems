3bd9a8d2e8346b9898077966092a4c1f3cb81211
Darwin_A <168660469+darwina200404@users.noreply.github.com>
2025-05-25 14:47:01 +0530

    Time: 4 ms (73.57%), Space: 12.6 MB (13.41%) - LeetHub

---
+++ 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def maxFreqSum(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        freq=Counter(s)
+        vowel={'a','e','i','o','u'}
+        maxVowel,maxConstant=0,0
+        for key,value in freq.items():
+            if key in vowel:
+                maxVowel=max(value,maxVowel)
+            else:
+                maxConstant=max(value,maxConstant)
+        return maxVowel+maxConstant
+
+
+        
